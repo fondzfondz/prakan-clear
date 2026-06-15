b1b1c86b9656464a1184bdf8d88e81193ccae9a6
fondzraknaa-crypto <fondzraknaa@gmail.com>
2026-06-15 19:52:03 +0700

    simple test

--- api/analyze-insurance.js
+++ api/analyze-insurance.js
@@ -1,7 +1,5 @@
-import { GoogleGenAI } from "@google/genai";
-
 export default async function handler(req, res) {
   return res.status(200).json({
-    keyExists: !!process.env.GEMINI_API_KEY
+    hello: "test"
   });
 }
