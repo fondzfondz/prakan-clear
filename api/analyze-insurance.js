0b4bd092984c0b036522d62e84d2a040cf890fd8
fondzraknaa-crypto <fondzraknaa@gmail.com>
2026-06-15 19:48:49 +0700

    check key

--- api/analyze-insurance.js
+++ api/analyze-insurance.js
@@ -1,22 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 export default async function handler(req, res) {
-  try {console.log(req.method);
-    const ai = new GoogleGenAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    });
-
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: "สวัสดี Gemini",
-    });
-
-    return res.status(200).json({
-      result: response.text,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-    });
-  }
+  return res.status(200).json({
+    keyExists: !!process.env.GEMINI_API_KEY
+  });
 }
